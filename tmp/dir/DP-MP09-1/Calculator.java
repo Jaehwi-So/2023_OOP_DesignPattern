5ffc863ce7869c90c2d94e8c60d0395d4d031389
Jaehwi-So <sjh9708@gmail.com>
2023-12-20 08:44:20 +0900

    add hw

---
+++ tmp/dir/DP-MP09-1/Calculator.java
@@ -0,0 +1,99 @@
+public class Calculator {
+    String operand1Str;
+    String operand2Str;
+    char op;
+    int op1Int;
+    int op2Int;
+    int result;
+
+
+    State state;
+    State start;
+    State operand1;
+    State operand2;
+    State operator;
+
+    public Calculator() {
+        start = new Start(this);
+        operand1 = new Operand1(this);
+        operand2 = new Operand2(this);
+        operator = new Operator(this);
+        state = start;
+        operand1Str = "";
+        operand2Str = "";
+    }
+    public void processDigit(int digit) {
+        state.processDigit(digit);
+    }
+
+    public void processArithmeticOperator(char ch) {
+        state.processArithmeticOperator(ch);
+    }
+
+    public void processEqualOperator() {
+        state.processEqualOperator();
+    }
+
+    public void setOperand1(int digit) {
+         operand1Str += digit;
+    }
+
+    public void setOperand2(int digit) {
+        operand2Str += digit;
+    }
+
+    public void setOperator(char ch) {
+        op = ch;
+    }
+
+    public void changeToOperand1State() {
+        state = operand1;
+    }
+
+    public void changeToOperand2State() {
+        state = operand2;
+    }
+
+    public void changeToOperatorState() {
+        state = operator;
+        op1Int = Integer.parseInt(operand1Str);
+    }
+
+    public void changeToStartState() {
+        state = start;
+        op2Int = Integer.parseInt(operand2Str);
+        calc();
+    }
+
+    public void calc() {
+        switch (op) {
+            case '+':
+                result = op1Int + op2Int;
+                break;
+            case '-':
+                result = op1Int - op2Int;
+                break;
+            case '*':
+                result = op1Int * op2Int;
+                break;
+            case '/':
+                result = op1Int / op2Int;
+                break;
+            case '%':
+                result = op1Int % op2Int;
+                break;
+        }
+        reset();
+    }
+
+    public int getResult() { return result; }
+
+    public int getOperand1() { return op1Int; }
+    public int getOperand2() { return op2Int; }
+
+    public char getOperator() { return op; }
+    private void reset() {
+        operand1Str = "";
+        operand2Str = "";
+    }
+}
---
+++ tmp/w/tm/DP-MP09-1/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+// 입력된 문자 ch가 0~9까지의 숫자를 나타내는 문자인 경우, ch - '0'으로 0~9까지의 정수로 변환 가능
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Calculator calc = new Calculator();
+
+        char ch = sc.next().charAt(0);      // 키보드에서 한 글자 입력 받기
+        while (ch != 'q' && ch != 'Q') {    // 종료 문자가 아니면 반복
+            if (ch == '=') {
+                calc.processEqualOperator();
+                System.out.printf("%d %c %d = %d\n",
+                        calc.getOperand1(), calc.getOperator(),
+                        calc.getOperand2(), calc.getResult());
+            }
+            else if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%') {
+                calc.processArithmeticOperator(ch);
+            }
+            else {
+                calc.processDigit(ch - '0');
+            }
+            ch = sc.next().charAt(0);      // 키보드에서 한 글자 입력 받기
+        }
+    }
+}
